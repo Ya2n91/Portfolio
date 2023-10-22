73a1a49523f90a31d9f19d8eabde1f989fc752cb
Ya2n91 <147325252+Ya2n91@users.noreply.github.com>
2023-10-22 20:41:40 +0200

    Add files via upload

---
+++ tp7.js
@@ -0,0 +1,7 @@
+function addition(){
+    var a = document.getElementById("t1").ariaValueMax;
+    var b = document.getElementById("t2").ariaValueMax;
+    var c = Number(a)+Number(b);
+
+    t3.value = c;
+}
