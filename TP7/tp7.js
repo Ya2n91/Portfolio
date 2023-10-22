728c886213abefe776b10830d468e9d89a14d611
Ya2n91 <147325252+Ya2n91@users.noreply.github.com>
2023-10-22 20:40:58 +0200

    Delete TP7 directory

--- TP7/tp7.js
+++
@@ -1,7 +0,0 @@
-function addition(){
-    var a = document.getElementById("t1").ariaValueMax;
-    var b = document.getElementById("t2").ariaValueMax;
-    var c = Number(a)+Number(b);
-
-    t3.value = c;
-}
